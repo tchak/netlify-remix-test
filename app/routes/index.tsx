c49978cebeb6e00654ffa4ed2cb7353b82292d6b
Paul Chavard <github@paul.chavard.net>
2021-09-05 15:53:58 +0200

    Apply prettier

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,28 +1,28 @@
-import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
-import { useRouteData } from "remix";
+import type { MetaFunction, LinksFunction, LoaderFunction } from 'remix';
+import { useRouteData } from 'remix';
 
-import stylesUrl from "../styles/index.css";
+import stylesUrl from '../styles/index.css';
 
 export let meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
+    title: 'Remix Starter',
+    description: 'Welcome to remix!',
   };
 };
 
 export let links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: stylesUrl }];
+  return [{ rel: 'stylesheet', href: stylesUrl }];
 };
 
 export let loader: LoaderFunction = async () => {
-  return { message: "this is awesome 😎" };
+  return { message: 'this is awesome 😎' };
 };
 
 export default function Index() {
   let data = useRouteData();
 
   return (
-    <div style={{ textAlign: "center", padding: 20 }}>
+    <div style={{ textAlign: 'center', padding: 20 }}>
       <h2>Welcome to Remix!</h2>
       <p>
         <a href="https://remix.run/dashboard/docs">Check out the docs</a> to get
